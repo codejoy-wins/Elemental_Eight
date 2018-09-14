3e3f61c56305893aa5268b574a2a37513a665630
Max <mjann@ucsc.edu>
2018-09-14 00:27:07 -0700

    confusing glitches

--- test.js
+++ test.js
@@ -1,15 +1,13 @@
-function sayHello(){
-    document.getElementById("final").innerHTML="HELLO";
-}
-function abra(){
+
+function kadabra(){
     var msg = document.getElementById("content").innerHTML;
-    msg+= " abra";
+    msg+= "Shaida is under attack";
     document.getElementById("content").innerHTML = msg;
     shaida.hurt();
 }
-function kadabra(){
+function abra(){
     var msg = document.getElementById("content").innerHTML;
-    msg+= " kadabra";
+    msg+= "Electross is under attack";
     document.getElementById("content").innerHTML = msg;
     electross.hurt();
 }
@@ -25,7 +23,9 @@ function elemental(name, type, moves, health){
 }
 
 var electross = new elemental("Electroman", "Electric", {"Thunderbolt": 50, "Thunderball":10}, 100);
+// var shaida = new elemental("Shaida", "Water", {"Waterslide": 65, "Waterfall":20, "Ice Cannon":100}, 160); why would ice cannon break?
 var shaida = new elemental("Shaida", "Water", {"Waterslide": 65, "Waterfall":20}, 160);
+
 // contructed two, now i need their methods prototypes
 
 elemental.prototype.report = function(){
@@ -53,33 +53,55 @@ elemental.prototype.hurt = function(){
     this.report();
     return this;
 }
-
-
+elemental.prototype.attack = function(enemy, move, amount){
+    var poison = amount;
+    console.log("enemy is: "+ enemy.name + " and move used is: "+ move + " and the poison done is " + poison);
+    enemy.health -= amount;
+    document.getElementById("content").innerHTML+= enemy.name + " was hit by " + move;
+    return this;
+}
 electross.report();
 shaida.report();
 
-// function reporter(){
-//     function elemental(name, type, moves){
-//         this.name = name;
-//         this.type = type;
-//         this.moves = moves;
-//     }
-    
-//     var electross = new elemental("Electroman", "Electric", {"Thunderbolt": 50});
-//     var shaida = new elemental("Electroman", "Electric", {"Waterslide": 65});
-//     // contructed two, now i need their methods prototypes
-    
-//     elemental.prototype.report = function(){
-//         console.log("prototyping");
-//         var m = "hello"
-//         m+= this.name;
-//         m+= this.type;
-//         m+= this.moves;
-//         document.getElementById("content").innerHTML= m;
-//         return this;
-//     }
-    
-//     electross.report();
-// }
+function sayHello(){
+    document.getElementById("final").innerHTML="HELLO";
+}
 
-// electricity, psymon, girl, shape of water, infernando, 
+for(z in shaida.moves){
+    document.getElementById("moveset").innerHTML+= "<button onclick=damage("+z+") id = "+z+" value ="+z+">"+z+"</button>";
+}
+
+for(x in electross.moves){
+    document.getElementById("moveset").innerHTML+= "<button onclick=damage2("+x+") id = "+x+" value ="+x+">"+x+"</button>";
+}
+
+function damage(para){
+    if(shaida.health<=0){
+        shaida.report();
+        return;
+    }
+    console.log("the damage is done to electross");
+    console.log(para);
+    var beast = para.value;
+    console.log(beast, "beast mode");
+    var amount = shaida.moves[beast];
+    shaida.attack(electross,beast,amount);
+    console.log(amount, "godforsaken amount");
+    electross.report();
+}
+function damage2(para){
+    if(electross.health<=0){
+        electross.report();
+        return;
+    }
+    console.log("the damage is done to shaida");
+    console.log(para);
+    var beast = para.value;
+    console.log(beast, "beast mode");
+    var amount = electross.moves[beast];
+    electross.attack(shaida,beast,amount);
+    console.log(amount, "godforsaken amount");
+    shaida.report();
+}
+
+// console.log(shaida.moves["Waterslide"]);
