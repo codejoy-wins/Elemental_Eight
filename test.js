ff8e4019e2fdb5ad71a68a7761f9f7553fa90f99
Max <mjann@ucsc.edu>
2018-09-14 00:35:41 -0700

    clarity

--- test.js
+++ test.js
@@ -24,7 +24,7 @@ function elemental(name, type, moves, health){
 
 var electross = new elemental("Electroman", "Electric", {"Thunderbolt": 50, "Thunderball":10}, 100);
 // var shaida = new elemental("Shaida", "Water", {"Waterslide": 65, "Waterfall":20, "Ice Cannon":100}, 160); why would ice cannon break?
-var shaida = new elemental("Shaida", "Water", {"Waterslide": 65, "Waterfall":20}, 160);
+var shaida = new elemental("Shaide", "Water", {"Waterslide": 65, "Waterfall":20}, 160);
 
 // contructed two, now i need their methods prototypes
 
@@ -34,7 +34,7 @@ elemental.prototype.report = function(){
         m+=x;
         m+=" "
     }
-    var full_report = "The name is: " + this.name + " and the type is: " + this.type + " and the moves are: " + m + "and the health is: " + this.health;
+    var full_report = this.name + " of type: " + this.type + " and the moves are: " + m + "and the health is: " + this.health;
     
     console.log(this.moves);
     console.log(m);
@@ -66,10 +66,12 @@ shaida.report();
 function sayHello(){
     document.getElementById("final").innerHTML="HELLO";
 }
+document.getElementById("moveset").innerHTML+= "Shaide's moves: ";
 
 for(z in shaida.moves){
     document.getElementById("moveset").innerHTML+= "<button onclick=damage("+z+") id = "+z+" value ="+z+">"+z+"</button>";
 }
+document.getElementById("moveset").innerHTML+= " Electross's moves: ";
 
 for(x in electross.moves){
     document.getElementById("moveset").innerHTML+= "<button onclick=damage2("+x+") id = "+x+" value ="+x+">"+x+"</button>";
